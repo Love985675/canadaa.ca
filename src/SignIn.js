7beaff687eaf31bba75baa01b806027d8ebd6927
Lovekesh Singh <89568022+Love985675@users.noreply.github.com>
2024-12-14 21:11:27 +0530

    done

--- src/SignIn.js
+++ src/SignIn.js
@@ -4,21 +4,21 @@ import NavBar2 from "./NavBar2";
 import { useNavigate } from "react-router-dom";
 
 const SignIn = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     console.log("Email:", email, "Password:", password);
-//     // Add logic to handle sign-in here
-//   };
-const navigate = useNavigate();
-const handleContinue = () => {
-  // Add a delay of 2 seconds (2000 milliseconds)
-  setTimeout(() => {
-    navigate("/welcome");
-  }, 3000);
-};
+  //   const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     console.log("Email:", email, "Password:", password);
+  //     // Add logic to handle sign-in here
+  //   };
+  const navigate = useNavigate();
+  const handleContinue = () => {
+    // Add a delay of 2 seconds (2000 milliseconds)
+    setTimeout(() => {
+      navigate("/welcome");
+    }, 3000);
+  };
 
   return (
     <div>
@@ -40,39 +40,49 @@ const handleContinue = () => {
         {/* Left Side: Sign-In Form */}
         <div className="sign-in-left">
           <h1>Sign In</h1>
-          <form  className="sign-in-form">
+          <form className="sign-in-form">
             <div className="form-group">
-              <label htmlFor="email"><p>Username:<bold>(required)</bold></p></label>
+              <label htmlFor="email">
+                <p>
+                  Username:<bold>(required)</bold>
+                </p>
+              </label>
               <input
                 type="username"
                 id="username"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                // value={email}
+                // onChange={(e) => setEmail(e.target.value)}
                 placeholder="username"
                 required
               />
             </div>
             <div className="form-group">
-              <label htmlFor="password"><p>Password:(required)</p></label>
+              <label htmlFor="password">
+                <p>Password:(required)</p>
+              </label>
               <input
                 type="password"
                 id="pass"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                // value={password}
+                // onChange={(e) => setPassword(e.target.value)}
                 placeholder="password"
                 required
               />
               <a href="">Forget your password? </a>
             </div>
-            <div className="buttongroup">
-              <button type="submit" className="sign-in-button" onClick={() => handleContinue("/welcome")}>
+            
+          </form>
+          <div className="buttongroup">
+              <button
+                className="sign-in-button"
+                onClick={() => handleContinue("/welcome")}
+              >
                 Sign In
               </button>
               <button type="clear" className="clear">
-               Clear All
+                Clear All
               </button>
             </div>
-          </form>
         </div>
 
         {/* Divider */}
@@ -85,10 +95,14 @@ const handleContinue = () => {
             A simple way to securely access Government of Canada online
             services.
           </p>
-          <p>One username.</p><p> One password.</p>
+          <p>One username.</p>
+          <p> One password.</p>
           <button className="sign-up-button">Sign Up</button>
-<br></br>
-          <p>your GCKey can be used to access multiple Government of Canada online Enabled Services</p>
+          <br></br>
+          <p>
+            your GCKey can be used to access multiple Government of Canada
+            online Enabled Services
+          </p>
         </div>
       </div>
     </div>
